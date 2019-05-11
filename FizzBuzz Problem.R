ba21a69298b6a43159534677bcd154a4367ef29e
ZNathani <nathanizainab@gmail.com>
2019-05-11 11:22:01 -0400

    Made some title changes

---
+++ FizzBuzz Problem.R
@@ -0,0 +1,24 @@
+num <-1
+seq<- c(1:100)
+for(i in seq) {
+  if (num%%3==0 && num%%5==0){
+      seq[num]<-"FizzBuzz"
+    }
+  else if (num%%3==0 & num%%5!=0){
+      seq[num]<-"Fizz"
+    }
+  else if (num%%3!=0 & num%%5==0){
+      seq[num]<-"Buzz"
+    }
+  else {
+      seq[num]<-num
+    }
+  num <- num+1
+    }
+print(seq)
+
+
+
+
+
+    
